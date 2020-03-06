9fd2ed98a906c62096600d249b6bf9c7eebb273b
Charles Adkins <cpeadkins@gmail.com>
2020-03-06 13:06:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,14 +4,6 @@ def countdown(n)
   while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
-
-def countdown(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
     def countdown_with_sleep(n)
       sleep(n)
     end
